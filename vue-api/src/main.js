96ea6d8f471d3b8fcff1acccfb65e63810bf2f14
731466760@qq.com <731466760@qq.com>
2019-02-12 18:26:39 +0800

    配置第三方icon库 并在main中引入 并在初始页面展示

--- vue-api/src/main.js
+++ vue-api/src/main.js
@@ -1,15 +1,20 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App';
+import router from './router';
 
-Vue.config.productionTip = false
+import '../src/assets/css/base.css'; // 初始化样式
+import '../src/assets/icon/iconfont'; // 引入svg iconfont js 
+import './assets/icon/iconfont.css'; // 引入 iconfont.css
+Vue.config.productionTip = false;
+
+Vue.use(ElementUI);
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
+});
