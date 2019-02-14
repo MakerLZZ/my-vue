a82d4a26cd3d2b758872e1bf5a3e0e69c7d61127
731466760@qq.com <731466760@qq.com>
2019-02-14 19:18:25 +0800

    构建项目初始页面

---
+++ vue-api/src/views/main.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="hello">
+        <el-button
+            type="primary"
+            icon="el-icon-api-vue"
+        >主要按钮</el-button>
+        <icon class="el-icon-api-vue"></icon>
+        <svg
+            class="icon"
+            aria-hidden="true"
+        >
+            <use xlink:href="#el-icon-api-vue"></use>
+        </svg>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'main',
+    data () {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        };
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+    font-weight: normal;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+a {
+    color: #42b983;
+}
+</style>
