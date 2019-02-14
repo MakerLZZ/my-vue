76a6e8f48608af4c4c6e78dbc19047ecf7b3fe20
731466760@qq.com <731466760@qq.com>
2019-02-14 19:17:19 +0800

    增加页面快速获取的字符串

--- vue-api/build/webpack.base.conf.js
+++ vue-api/build/webpack.base.conf.js
@@ -26,6 +26,7 @@ module.exports = {
         alias: {
             'vue$': 'vue/dist/vue.esm.js',
             '@': resolve('src'),
+            '_v': resolve('src/views'),
         }
     },
     module: {
