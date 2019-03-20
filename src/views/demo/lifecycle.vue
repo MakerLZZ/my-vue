fa0aa40e7dc5419ca1d0b6406288cceb6c7ca055
731466760@qq.com <731466760@qq.com>
2019-03-20 20:28:37 +0800

    增加生命周期模块

--- src/views/demo/lifecycle.vue
+++ src/views/demo/lifecycle.vue
@@ -23,80 +23,143 @@
             >生命周期图示</a>
         </p>
         <h3
-            id="beforeCreate"
-            data-scroll-id="beforeCreate"
+            id="create-mount"
+            data-scroll-id="create-mount"
             tabindex="-1"
             style="outline: none;"
         ><a
-                href="#beforeCreate"
+                href="#create-mount"
                 class="headerlink"
-                title="beforeCreate"
+                title="create-mount"
                 data-scroll=""
-            >beforeCreate/created</a></h3>
+            >create/mount</a></h3>
+        <ul>
+            <!-- `&nbsp;&nbsp;&nbsp;&nbsp;\n` + 
+                `\t\n` +
+                `&nbsp;&nbsp;&nbsp;&nbsp;},\n` +  -->
+            <li>
+                <p><strong>示例：</strong></p>
+                <pre><code class="hljs js">{{
+                `export default {\n` + 
+                `&nbsp;&nbsp;&nbsp;&nbsp;beforeCreate() {\n` + 
+                `\tconsole.group('beforeCreate 创建前状态===============》');\n` + 
+                `\tconsole.log('%c%s', 'color:red', 'el     : ' + this.$el); // undefined\n` + 
+                `\tconsole.log('%c%s', 'color:red', 'data   : ' + this.$data); // undefined\n` + 
+                `\tconsole.log('%c%s', 'color:red', 'message: ' + this.message);\n` + 
+                `&nbsp;&nbsp;&nbsp;&nbsp;},\n` + 
+                `&nbsp;&nbsp;&nbsp;&nbsp;created() {\n` + 
+                `\tconsole.group('created 创建完毕状态===============》');\n` +
+                `\tconsole.log('%c%s', 'color:red', 'el     : ' + this.$el); // undefined\n` +
+                `\tconsole.log('%c%s', 'color:red', 'data   : ' + this.$data); // 已被初始化\n` +
+                `\tconsole.log('%c%s', 'color:red', 'message: ' + this.message); // 已被初始化\n` +
+                `&nbsp;&nbsp;&nbsp;&nbsp;},\n` + 
+                `&nbsp;&nbsp;&nbsp;&nbsp;beforeMount() {\n` + 
+                `\tconsole.group('beforeMount 挂载前状态===============》');\n` +
+                `\tconsole.log('%c%s', 'color:red', 'el     : ' + this.$el); // 已被初始化\n` +
+                `\tconsole.log(this.$el);\n` +
+                `\tconsole.log('%c%s', 'color:red', 'data   : ' + this.$data); // 已被初始化\n` +
+                `\tconsole.log('%c%s', 'color:red', 'message: ' + this.message); // 已被初始化\n` +
+                `&nbsp;&nbsp;&nbsp;&nbsp;},\n` + 
+                `&nbsp;&nbsp;&nbsp;&nbsp;mounted() {\n` + 
+                `\tconsole.group('mounted 挂载结束状态===============》');\n` +
+                `\tconsole.log('%c%s', 'color:red', 'el     : ' + this.$el); // 已被初始化\n` +
+                `\tconsole.log(this.$el);\n` +
+                `\tconsole.log('%c%s', 'color:red', 'data   : ' + this.$data); // 已被初始化\n` +
+                `\tconsole.log('%c%s', 'color:red', 'message: ' + this.message); // 已被初始化\n` +
+                `&nbsp;&nbsp;&nbsp;&nbsp;}\n` + 
+                `}`}}</code></pre>
+            </li>
+            <li>
+                <p><strong>结果：</strong></p>
+                <p><img src="../../assets/img/demo/life-c-m.png"></p>
+            </li>
+            <li>
+                <p><strong>分析：</strong></p>
+                <p><code>beforeCreat</code>钩子到<code>created</code>钩子之间开始监控Data对象数据变化，在<code>created</code>时Data对象已初始化</p>
+                <p><code>beforeMount</code>钩子时dom还是{ { message } }，这里运用虚拟dom技术，在<code>mounted</code>挂载时再把值渲染进去</p>
+            </li>
+        </ul>
+        <h3
+            id="update"
+            data-scroll-id="update"
+            tabindex="-1"
+            style="outline: none;"
+        ><a
+                href="#update"
+                class="headerlink"
+                title="update"
+                data-scroll=""
+            >update</a></h3>
+        <ul>
+            <li>
+                <p><strong>示例：</strong></p>
+                <pre><code class="hljs js">{{
+                `export default {\n` +
+                `&nbsp;&nbsp;&nbsp;&nbsp;beforeUpdate() {\n` +
+                `\tconsole.group('beforeUpdate 更新前状态===============》');\n` +
+                `\tconsole.log('%c%s', 'color:red', 'el     : ' + this.$el);\n` +
+                `\tconsole.log(this.$el);\n` +
+                `\tconsole.log('%c%s', 'color:red', 'data   : ' + this.$data);\n` +
+                `\tconsole.log('%c%s', 'color:red', 'message: ' + this.message);\n` +
+                `&nbsp;&nbsp;&nbsp;&nbsp;},\n` +
+                `&nbsp;&nbsp;&nbsp;&nbsp;updated() {\n` + 
+                `\tconsole.group('updated 更新完成状态===============》');\n` +
+                `\tconsole.log('%c%s', 'color:red', 'el     : ' + this.$el);\n` +
+                `\tconsole.log(this.$el);\n` +
+                `\tconsole.log('%c%s', 'color:red', 'data   : ' + this.$data);\n` +
+                `\tconsole.log('%c%s', 'color:red', 'message: ' + this.message);\n` +
+                `&nbsp;&nbsp;&nbsp;&nbsp;}\n` +
+                `}`}}</code></pre>
+            </li>
+            <li>
+                <p><strong>结果：</strong></p>
+                <p><img src="../../assets/img/demo/life-u.png"></p>
+            </li>
+            <li>
+                <p><strong>分析：</strong></p>
+                <p>数据变化了dom随时更新</p>
+            </li>
+        </ul>
+        <h3
+            id="destroy"
+            data-scroll-id="destroy"
+            tabindex="-1"
+            style="outline: none;"
+        ><a
+            href="#destroy"
+            class="headerlink"
+            title="destroy"
+            data-scroll=""
+        >destroy</a></h3>
+        <ul>
+            <li>
+                <p><strong>示例：</strong></p>
+                <pre><code class="hljs js">{{
+                `export default {\n` +
+                `&nbsp;&nbsp;&nbsp;&nbsp;beforeDestroy() {\n` +
+                `\tconsole.group('beforeDestroy 销毁前状态===============》');\n` +
+                `\tconsole.log('%c%s', 'color:red', 'el     : ' + this.$el);\n` +
+                `\tconsole.log(this.$el);\n` +
+                `\tconsole.log('%c%s', 'color:red', 'data   : ' + this.$data);\n` +
+                `\tconsole.log('%c%s', 'color:red', 'message: ' + this.message);\n` +
+                `&nbsp;&nbsp;&nbsp;&nbsp;},\n` +
+                `&nbsp;&nbsp;&nbsp;&nbsp;destroyed() {\n` + 
+                `\tconsole.group('destroyed 销毁完成状态===============》');\n` +
+                `\tconsole.log('%c%s', 'color:red', 'el     : ' + this.$el);\n` +
+                `\tconsole.log(this.$el);\n` +
+                `\tconsole.log('%c%s', 'color:red', 'data   : ' + this.$data);\n` +
+                `\tconsole.log('%c%s', 'color:red', 'message: ' + this.message);\n` +
+                `&nbsp;&nbsp;&nbsp;&nbsp;}\n` +
+                `}`}}</code></pre>
+            </li>
+            <li>
+                <p><strong>结果：</strong></p>
+                <p><img src="../../assets/img/demo/life-d.png"></p>
+            </li>
+            <li>
+                <p><strong>分析：</strong></p>
+                <p><code>beforeDestroy</code>vue实例销毁前执行的钩子，<code>destroyed</code>vue实例销毁</p>
+            </li>
+        </ul>
     </div>
 </template>
-
-<script>
-export default {
-    data() {
-        return {
-            message: 'test'
-        };
-    },
-    beforeCreate: function () {
-        console.group('beforeCreate 创建前状态===============》');
-        console.log('%c%s', 'color:red', 'el     : ' + this.$el); //undefined
-        console.log('%c%s', 'color:red', 'data   : ' + this.$data); //undefined 
-        console.log('%c%s', 'color:red', 'message: ' + this.message);
-    },
-    created: function () {
-        console.group('created 创建完毕状态===============》');
-        console.log('%c%s', 'color:red', 'el     : ' + this.$el); //undefined
-        console.log('%c%s', 'color:red', 'data   : ' + this.$data); //已被初始化 
-        console.log('%c%s', 'color:red', 'message: ' + this.message); //已被初始化
-    },
-    beforeMount: function () {
-        console.group('beforeMount 挂载前状态===============》');
-        console.log('%c%s', 'color:red', 'el     : ' + this.$el); //已被初始化
-        console.log(this.$el);
-        console.log('%c%s', 'color:red', 'data   : ' + this.$data); //已被初始化  
-        console.log('%c%s', 'color:red', 'message: ' + this.message); //已被初始化  
-    },
-    mounted: function () {
-        console.group('mounted 挂载结束状态===============》');
-        console.log('%c%s', 'color:red', 'el     : ' + this.$el); //已被初始化
-        console.log(this.$el);
-        console.log('%c%s', 'color:red', 'data   : ' + this.$data); //已被初始化
-        console.log('%c%s', 'color:red', 'message: ' + this.message); //已被初始化 
-    },
-    beforeUpdate: function () {
-        console.group('beforeUpdate 更新前状态===============》');
-        console.log('%c%s', 'color:red', 'el     : ' + this.$el);
-        console.log(this.$el);
-        console.log('%c%s', 'color:red', 'data   : ' + this.$data);
-        console.log('%c%s', 'color:red', 'message: ' + this.message);
-    },
-    updated: function () {
-        console.group('updated 更新完成状态===============》');
-        console.log('%c%s', 'color:red', 'el     : ' + this.$el);
-        console.log(this.$el);
-        console.log('%c%s', 'color:red', 'data   : ' + this.$data);
-        console.log('%c%s', 'color:red', 'message: ' + this.message);
-    },
-    beforeDestroy: function () {
-        console.group('beforeDestroy 销毁前状态===============》');
-        console.log('%c%s', 'color:red', 'el     : ' + this.$el);
-        console.log(this.$el);
-        console.log('%c%s', 'color:red', 'data   : ' + this.$data);
-        console.log('%c%s', 'color:red', 'message: ' + this.message);
-    },
-    destroyed: function () {
-        console.group('destroyed 销毁完成状态===============》');
-        console.log('%c%s', 'color:red', 'el     : ' + this.$el);
-        console.log(this.$el);
-        console.log('%c%s', 'color:red', 'data   : ' + this.$data);
-        console.log('%c%s', 'color:red', 'message: ' + this.message);
-    }
-};
-</script>
-
