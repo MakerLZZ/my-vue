e04fdc42df72d59ef86accad56747365ffda919c
731466760@qq.com <731466760@qq.com>
2019-03-20 20:31:31 +0800

    增加render-error demo

---
+++ src/views/test/render-error.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="test"></div>
+</template>
+
+<script>
+import Vue from 'vue';
+export default {
+    mounted() {
+        var FatherComponent = Vue.extend({
+            template: '<span>test</span>'
+        });
+        new FatherComponent({
+            render () { // render (h) {
+                throw new Error('test');
+            },
+            renderError (h, err) {
+                return h('p', { style: { color: 'red' }}, err.stack);
+            }
+        }).$mount('#test');
+    }
+
+};
+</script>
