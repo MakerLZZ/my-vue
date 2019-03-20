a92f62adc7764477a9f22af4607dbea675b48871
731466760@qq.com <731466760@qq.com>
2019-03-20 20:29:51 +0800

    增加项目搭建模块

---
+++ src/views/demo/project.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2 id="项目搭建"><a
+                href="#项目搭建"
+                class="headerlink"
+                title="项目搭建"
+                data-scroll=""
+            >项目搭建</a></h2>
+        <ul>
+            <li>
+                <p><strong>项目快速搭建准备：</strong></p>
+                <p>安装node，npm</p>
+                <p>安装webpack：npm install webpack -g</p>
+                <p>安装vue-cli：npm install vue-cli -g 参考
+                    <a
+                        href="https://cli.vuejs.org/zh/"
+                        target="_blank"
+                    >API地址</a>
+                </p>
+            </li>
+            <li>
+                <p><strong>vue-cli构建项目：</strong></p>
+            </li>
+        </ul>
+    </div>
+</template>
