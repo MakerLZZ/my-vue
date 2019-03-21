52a22461e4aa21d28d531423923887c2fbd6bb48
731466760@qq.com <731466760@qq.com>
2019-03-21 21:39:06 +0800

    增加组件模块 完善项目构建模块

---
+++ src/views/demo/_component.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <h2 id="组件"><a
+                href="#组件"
+                class="headerlink"
+                title="组件"
+                data-scroll=""
+            >组件</a></h2>
+        <h3
+            id="com-init"
+            data-scroll-id="com-init"
+            tabindex="-1"
+            style="outline: none;"
+        ><a
+                href="#com-init"
+                class="headerlink"
+                title="com-init"
+                data-scroll=""
+            >组件注册父与子</a></h3>
+        <ul>
+            <li>
+                <p><strong>父组件：</strong></p>
+                <pre><code class="hljs js">{{
+                `&#60;template&#62;\n` +
+                `&nbsp;&nbsp;&nbsp;&nbsp;&#60;Child :data="message" @father-method="handleCallback" /&#62;\n` +
+                `&#60;/template&#62;\n` +
+                `&#60;script&#62;\n` +
+                `import Child from './Child'\n` +
+                `export default {\n` +
+                `&nbsp;&nbsp;&nbsp;&nbsp;components: {\n` +
+                `\tChild\n` +
+                `&nbsp;&nbsp;&nbsp;&nbsp;},\n` +
+                `&nbsp;&nbsp;&nbsp;&nbsp;data() {\n` +
+                `\tmessage: '来自父组件'\n` +
+                `&nbsp;&nbsp;&nbsp;&nbsp;},\n` +
+                `&nbsp;&nbsp;&nbsp;&nbsp;methods: {\n` +
+                `\thandleCallback(val) {\n` +
+                `\t&nbsp;&nbsp;&nbsp;&nbsp;console.log(val) // 处理子组件的回调\n` +
+                `\t}\n` +
+                `&nbsp;&nbsp;&nbsp;&nbsp;}\n` +
+                `}\n` +
+                `&#60;script&#62;`}}</code></pre>
+            </li>
+            <li>
+                <p><strong>子组件：</strong></p>
+                <pre><code class="hljs js">{{
+                `&#60;template&#62;\n` +
+                `&nbsp;&nbsp;&nbsp;&nbsp;&#60;input :value="message" @change="handleChange" /&#62;\n` +
+                `&#60;/template&#62;\n` +
+                `&#60;script&#62;\n` +
+                `export default {\n` +
+                `&nbsp;&nbsp;&nbsp;&nbsp;props: ['message']\n` +
+                `&nbsp;&nbsp;&nbsp;&nbsp;methods: {\n` +
+                `\thandleChange(val) {\n` +
+                `\t&nbsp;&nbsp;&nbsp;&nbsp;this.$emit('father-method', val);\n` +
+                `\t}\n` +
+                `&nbsp;&nbsp;&nbsp;&nbsp;}\n` +
+                `}\n` +
+                `&#60;script&#62;`}}</code></pre>
+            </li>
+        </ul>
+    </div>
+</template>
--- src/views/demo/project.vue
+++ src/views/demo/project.vue
@@ -6,9 +6,20 @@
                 title="项目搭建"
                 data-scroll=""
             >项目搭建</a></h2>
+        <h3
+            id="project-ready"
+            data-scroll-id="project-ready"
+            tabindex="-1"
+            style="outline: none;"
+        ><a
+                href="#project-ready"
+                class="headerlink"
+                title="project-ready"
+                data-scroll=""
+            >项目准备</a></h3>
         <ul>
             <li>
-                <p><strong>项目快速搭建准备：</strong></p>
+                <p><strong>相关命令：</strong></p>
                 <p>安装node，npm</p>
                 <p>安装webpack：npm install webpack -g</p>
                 <p>安装vue-cli：npm install vue-cli -g 参考
@@ -18,8 +29,54 @@
                     >API地址</a>
                 </p>
             </li>
+        </ul>
+        <h3
+            id="project-create"
+            data-scroll-id="project-create"
+            tabindex="-1"
+            style="outline: none;"
+        ><a
+                href="#project-create"
+                class="headerlink"
+                title="project-create"
+                data-scroll=""
+            >构建项目</a></h3>
+        <ul>
             <li>
-                <p><strong>vue-cli构建项目：</strong></p>
+                <p><strong>相关命令：</strong></p>
+                <p>构建项目（名称demo）：vue init webpack demo</p>
+                <p><code>? Project name (demo) </code>输入项目名称或直接回车确认名称</p>
+                <p><code>? Project description (A Vue.js project) </code>输入项目描述或者直接回车确认</p>
+                <p><code>? Author Maker</code>输入作者名称或直接回车确认</p>
+                <p><code>? Vue build (Use arrow keys)</code>选择Runtime + Compiler或Runtime-only</p>
+                <p><code>? Install vue-router? (Y/n)</code>根据需要选择是否配置路由</p>
+                <p><code>? Use ESlint to lint your code? (Y/n)</code>根据需要选择是否添加eslint</p>
+                <p><code>? Set up unit tests (Y/n)</code>根据需要选择是否配置单元测试</p>
+                <p><code>? Setup e2e tests with Nightwatch? (Y/n)</code>根据需要选择是否配置e2e测试</p>
+                <p><code>? Should we run 'npm Install' for you after the project has been creactd? (Use arrow keys)</code>根据需要选择打包工具</p>
+                <p><code>vue-cli · Generated "demo".</code>等待项目初始化...</p>
+                <p><code>npm install</code>安装依赖</p>
+                <p><code>npm run dev</code>本地运行项目</p>
+            </li>
+        </ul>
+        <h3
+            id="project-list"
+            data-scroll-id="project-list"
+            tabindex="-1"
+            style="outline: none;"
+        ><a
+                href="#project-list"
+                class="headerlink"
+                title="project-list"
+                data-scroll=""
+            >项目目录</a></h3>
+        <ul>
+            <li>
+                <p><strong>项目目录结构概览：</strong></p>
+                <p><img
+                        src="../../assets/img/demo/project-detail.png"
+                        alt=""
+                    ></p>
             </li>
         </ul>
     </div>
